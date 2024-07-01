fa8cd7adac1aa3cb223ceda153c78bd2a18af9c2
SpicyCoder <spk2naveen@hotmail.com>
2024-07-01 18:04:02 +0530

    Update local build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 dotnet clean -c Release .\CICDTemplate.sln
 dotnet restore .\CICDTemplate.sln
 dotnet build -c Release .\CICDTemplate.sln
-dotnet test -c Release .\CICDTemplate.sln /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./.coverage/ /p:ExcludeByFile="**/Migrations/*.cs" /p:Exclude="[CICDTemplate.ServiceDefaults]*"
+dotnet test -c Release .\CICDTemplate.sln --collect:"XPlat Code Coverage" --settings .\coverage.runsettings
 dotnet reportgenerator "-reports:./**/coverage.cobertura.xml" "-targetdir:./.coverage" -reporttypes:"Html_Dark;SonarQube"
 dotnet stryker --solution .\CICDTemplate.sln -r "html" -r "progress" -r "markdown"
